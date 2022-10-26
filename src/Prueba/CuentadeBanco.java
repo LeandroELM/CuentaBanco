0e0a99aa05b37fd6dd34bd5a41f94b5e066be181
Leandro Lacayo <leandrolacayo17@gmail.com>
2022-10-26 17:18:46 -0600

    Se agrego Cuenta de Banco

---
+++ src/Prueba/CuentadeBanco.java
@@ -0,0 +1,80 @@
+package Prueba;
+
+public class CuentadeBanco {
+    
+    	private int NodeCuenta;
+    	private int NoTelefono;
+    	private double saldo;
+    	private String NCliente,email;
+    	//Metodos funcionales
+    	public void Depositar(double montodeposito) {
+        	this.saldo += montodeposito;
+        	mostrarDatos();
+        	System.out.println("**********************************************");
+        	System.out.println("Realizando transaccion...");
+        	System.out.println("El monto depositado fue de C$" + montodeposito);
+        	System.out.println("Su saldo actual es de C$" + mostrarsaldo());
+        	System.out.println("**********************************************");
+    	}
+    	public void retirar(double montoretiro) {
+    		if(montoretiro > this.saldo) {
+    			mostrarDatos();
+    			System.out.println("**********************************************");
+    			System.out.println("Saldo insuficiente.");
+    			System.out.println("Su saldo actual es de C$" + mostrarsaldo());
+
+    	} else {
+    		System.out.println("**********************************************");
+    		System.out.println("Realizando transaccion...");
+    	    this.saldo -= montoretiro;
+    	    System.out.println("el monto retirado fue de C$" + montoretiro);
+    	    System.out.println("Su saldo actual es de C$" + mostrarsaldo());
+    	    System.out.println("**********************************************");
+    	}
+    	}
+    	public void mostrarDatos() {
+    		
+    		System.out.println("Nombre: " + mostrarNCliente());
+    		System.out.println("Numero de cuenta: " + mostrarNodeCuenta());
+    		System.out.println("Email: " + mostraremail());
+    		System.out.println("Telefono: " + mostrarNoTelefono());
+    	}
+    		
+    		
+    		
+    	
+    	 //Metodos setters
+    	public void ponerNodeCuenta(int NodeCuenta) {
+    		this.NodeCuenta=NodeCuenta;
+    	}
+    	public int mostrarNodeCuenta() {
+    		return this.NodeCuenta;
+    	}
+    	public void ponerNoTelefono(int NoTelefono) {
+    		this.NoTelefono=NoTelefono;
+    	}
+    	public int mostrarNoTelefono() {
+    		return this.NoTelefono;
+    	}
+    	public void ponersaldo(double saldo) {
+    		this.saldo=saldo;
+    	}
+    	public double mostrarsaldo() {
+    		return this.saldo;
+    	}
+    	public void ponerNCliente(String NCliente) {
+    		this.NCliente=NCliente;
+    	}
+    	public String mostrarNCliente() {
+    		return this.NCliente;
+    		
+    	}public void poneremail(String email) {
+    		this.email=email;
+    	}
+    	public String mostraremail() {
+    		return this.email;
+    	}
+    	
+    	
+     }
+
---
+++ src/Prueba/calculo.java
@@ -0,0 +1,25 @@
+package Prueba;
+
+public class calculo {
+
+	public static void main(String[] args) {
+		CuentadeBanco cuentaJuan=new CuentadeBanco();
+		
+		
+		
+		cuentaJuan.ponerNCliente("Juan Martinez");
+		cuentaJuan.poneremail("juan.martinez@gmail.com");
+		cuentaJuan.ponerNodeCuenta(65246546);
+		cuentaJuan.ponerNoTelefono(89654214);
+		
+		cuentaJuan.retirar(50.00);
+		cuentaJuan.Depositar(150.00);
+		cuentaJuan.Depositar(500.00);
+		cuentaJuan.retirar(50.00);
+		
+		CuentadeBanco cuentaMaria=new CuentadeBanco();
+		cuentaMaria.mostrarDatos();
+
+	}
+
+}
